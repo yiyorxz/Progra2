d71c393e2522a4d70cd8dfa4cba50d2e9cd20298
Claudio204g <c.gonzalez.162411@gmail.com>
2023-08-29 19:03:19 -0400

    agregar

---
+++ sistema.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+    while(true) {
+        cout << "hola bienvenido al sistema de vuelos el treile" << endl;
+        cout << "selecciona la operacion que deseas realizar" << endl;
+        cout << "opcion 1 " << std:: endl;
+        cout << "opcion 2 " << std:: endl;
+        cout << "opcion 3 " << std:: endl;
+        cout << "opcion 4 " << std:: endl;
+        cout << "opcion 5 " << std:: endl;
+
+        int valor;
+        cout<<"ingresa la opcion:" ;
+        cin >> valor;
+
+        if (valor == 5) {
+            break;
+
+        switch (valor) {
+
+        }
+        };
+
+    }
+}
