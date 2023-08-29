fbf3db4119d60332ea1efcf20666a6cdba76df4c
DoSand12 <ghost.1783r@gmail.com>
2023-08-29 19:22:54 -0400

    hello

---
+++ 28del8.py
@@ -0,0 +1,14 @@
+class Avion:
+    def __init__(self, modeloavion, num_asientos, vuelosin):
+        self.modeloavion = modeloavion
+        self.num_asientos = num_asientos
+        self.vuelosin = vuelosin
+
+        def __str__(self):
+            return 'Modeloavion: {}\nNum_asientos: {}nVuelo: {}'
+
+
+
+
+
+
