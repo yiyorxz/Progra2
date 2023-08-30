dbf68cfab876298d896a28acea680517b0eefb83
Pamtom21 <joluis.2.11.04@gmail.com>
2023-08-30 09:47:16 -0400

    jelo

--- 28del8.py
+++ 28del8.py
@@ -1,11 +1,12 @@
+from datetime import datetime
 class Avion:
     def __init__(self, modeloavion, asientos):
         self.modeloavion = modeloavion
         self.asientos = asientos
     def __str__(self):
-        return f"{self.modeloavion}"    
+        return f"{self.modeloavion} {self.asientos}"    
 class vuelo:
-     def __init__(self, nvuelo, origen, destino, fecha, hora, _avion ):
+     def __init__(self, nvuelo, origen, destino, fecha, hora , _avion ):
          self.nvuelo = nvuelo
          self.origen=origen
          self.destino= destino
@@ -19,7 +20,7 @@ class vuelo:
         self.reservaciones.append(_reserva)
      def mostrar_reservas(_reserva):
         print("reservas:")
-        for _reserva in vuelo_1.reservaciones:
+        for _reserva in vuelo.reservaciones:
             print(f"numero reserva {_reserva.nreserva} (Pasajero: {_reserva._pasajero}), (Vuelo: {_reserva._vuelo}) Estado: {_reserva.estado}")
 class Vuelos:
     def __init__(self):
@@ -28,52 +29,12 @@ class Vuelos:
     def Vuelos_disponibles(self, avuelo):
         self.Vuelos.append(avuelo)
 
-    def delete_vuelo(self, nvuelo):
-        for vuelo in self.Vuelos:
-            if vuelo.nvuelo == nvuelo:
-                confirmacion = input(f"¿Estás seguro de eliminar el vuelo {nvuelo}? (Y/N): ")
-                if confirmacion == "Y":
-                    self.Vuelos.remove(vuelo)
-                    print(f"El vuelo {nvuelo} ha sido eliminado.")
-                elif confirmacion == "N":
-                    print("Eliminación de vuelo cancelada.")
 
     def mostrar_vuelos(_vuelo):
         print("Vuelos disponibles:")
         for _vuelo in vuelos_1.Vuelos:
-            print(f"Proximos_vuelo: {_vuelo.nvuelo}, Lugar de Origen: {_vuelo.origen}, Lugar de Destino: {_vuelo.destino}, Fecha asignada: {_vuelo.fecha}, Hora de: {_vuelo.hora}, Avion asignado: {_vuelo._avion}"
+            print(f"vuelo: {_vuelo.nvuelo}, Lugar de Origen: {_vuelo.origen}, Lugar de Destino: {_vuelo.destino}, Fecha asignada: {_vuelo.fecha}, Hora de: {_vuelo.hora}, Avion asignado: {_vuelo._avion}"
                   "-------------------------------------------------------------------------------------------------------------------------------------------------------------------------")    
-class reservacion:
-    def __init__(self, nreserva, _pasajero, _vuelo, estado):
-        self.nreserva=nreserva
-        self._pasajero=_pasajero
-        self._vuelo=_vuelo
-        self.estado=estado
-
-        
-vuelos_1= Vuelos()
-
-avion_17 = Avion("bartolome 14", 60)
-avion_18 = Avion("bartolome 15", 60)
-avion_183 = Avion("bartolome 16", 60)
-avion_184 = Avion("bartolome 17", 60)
-avion_185 = Avion("bartolome 18", 60)
-
-vuelo_1 = vuelo(1,str(input("ingrese origen ")), str(input("ingrese destino ")), str(input("ingrese fecha")), str(input("ingrese hora ")),avion_17)
-vuelo_12 = vuelo(2,"santiago", "miami", "17/08/24", "17:40",avion_18)
-vuelo_13 = vuelo(3,"santiago", "miami", "17/08/24", "17:40",avion_183)
-vuelo_14 = vuelo(4,"miami", "peru causa", "17/08/24", "17:40",avion_184)
-
-vuelos_1.Vuelos_disponibles(vuelo_1)
-vuelos_1.Vuelos_disponibles(vuelo_12)
-vuelos_1.Vuelos_disponibles(vuelo_13)
-vuelos_1.Vuelos_disponibles(vuelo_14)
-
-Vuelos.mostrar_vuelos(vuelo_1)
-
-# Eliminar un vuelo
-vuelo_eliminar = int(input("Ingrese el número de vuelo que desea eliminar: "))
-vuelos_1.delete_vuelo(vuelo_eliminar)
 class Pasajero:
     def __init__(self, pasaporte, nombre):
         self.pasaporte = pasaporte
@@ -81,30 +42,34 @@ class Pasajero:
         self.vuelos_reservados = []
     def __str__(self):
         return f"{self.pasaporte}, {self.nombre}"
-    # def reservar(self,_reserva):
-    #     self.vuelos_reservados.append(_reserva)
-    
-    # def vuelo_reservado(self):
-    #     print(f"Vuelos reservados para{self.nombre} N°Pasaporte {self.pasaporte}:")
+class reservacion:
+    def __init__(self, nreserva, _pasajero, _vuelo, estado):
+        self.nreserva=nreserva
+        self._pasajero=_pasajero
+        self._vuelo=_vuelo
+        self.estado=estado
+listaaviones=[]
+def crear_aviones(x):
+    listaaviones=[]
+    for l in range(x):
+        listaaviones.append(l)
+    for i in range(0,x):
+        listaaviones[i-1]= Avion(str(input("ingrese nombre avion ")), str(input("ingrese numero de asientos ")))
+    print("aviones disponibles: ")
+    for c in range(len(listaaviones)):
+        print (f"Nombre del avion '{listaaviones[c].modeloavion}' Numero de asientos '{listaaviones[c].asientos}'")
+    return listaaviones
 
-    #     for vuelo in self.vuelos_reservados:
-    #         print(f"Número de vuelo: ______ {vuelo.nvuelo}, Origen:___ {vuelo.origen}, Destino: ___{vuelo.destino}, Fecha:__ {vuelo.fecha}, Hora: {vuelo.hora}")
+def crear_vuelos(x):
+    listavuelos=[]
+    for i in range(x):
+        listavuelos.append(i)
+    for l in range(0,x):
+        listavuelos[l]=vuelo(input("Ingrese numero de vuelo "),input("Ingrese lugar de origen"),input("Ingrese lugar de destino"),input("Ingrese fecha "),input("ingrese hora "),input(listaaviones[l-1]))
 
-
-pasajero_1 = Pasajero("AB123456", "Jose Ernandes")
-pasajero_2 = Pasajero("CD789012", "Maria Canalio")
-pasajero_3 = Pasajero("EF345678", "Nashe")
-
-# pasajero_1.reservacion_vuelo(vuelo_1)
-# pasajero_2.reservacion_vuelo(vuelo_12)
-# pasajero_3.reservacion_vuelo(vuelo_13)
-# pasajero_1.reservacion_vuelo(vuelo_14)
-
-reserva= reservacion(1, pasajero_1, vuelo_1, "reservado")
-vuelo.reservar(vuelo_1, reserva)
-vuelo.mostrar_reservas(reserva)
-
-
+        
+crear_aviones(3)
+crear_vuelos(3)
 
 
         
